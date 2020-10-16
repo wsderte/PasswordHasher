2139dd9e4bef4b47c3dd62e4e072aa547a8906e5
wsderte <31801595+wsderte@users.noreply.github.com>
2020-10-16 15:25:47 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,39 @@
+using System;
+using Xunit;
+using IIG.PasswordHashingUtils;
+
+namespace XUnitTestProject2 {
+    public class UnitTest1 {
+
+        [Fact]
+        public void TestGetHash() {
+
+            Assert.NotNull(PasswordHasher.GetHash("1111"));
+            Assert.Equal(PasswordHasher.GetHash("1111"), PasswordHasher.GetHash("1111"));
+            Assert.NotEqual(PasswordHasher.GetHash("1111"), PasswordHasher.GetHash("1111", "ww"));
+            Assert.NotEqual(PasswordHasher.GetHash("1111"), PasswordHasher.GetHash("1111",null,123));
+
+        }
+
+        [Fact]
+        public void TestInit() {
+
+            String a = PasswordHasher.GetHash("1111");
+            PasswordHasher.Init("wsd", 5);
+            Assert.NotEqual( a, PasswordHasher.GetHash("1111"));
+
+            PasswordHasher.Init("wsd", 0);
+            Assert.Equal(PasswordHasher.GetHash("1111"), PasswordHasher.GetHash("1111"));
+
+            PasswordHasher.Init(null, 6);
+            Assert.Equal(PasswordHasher.GetHash("1111"), PasswordHasher.GetHash("1111"));
+
+            a = PasswordHasher.GetHash("1111");
+            PasswordHasher.Init(null, 6);
+            Assert.Equal(a, PasswordHasher.GetHash("1111"));
+
+        }
+
+    }
+}
+
